e8c9de9deec069dafdb87749fd1ed7af881ecb26
George <g9c9@outlook.com>
2018-10-25 13:29:51 -0700

    Added game timer that gets paused

--- STABEL/Assets/Scripts/Manager.cs
+++ STABEL/Assets/Scripts/Manager.cs
@@ -9,6 +9,9 @@ public class Manager : MonoBehaviour {
     public Image heartImage;
     public GameObject InGameMenupanel;
     public Text buttonText;
+    public Text timerText;
+    public int timelimit;
+    private float time;
     private PlayerBehavior player;
     private Spawner spawner;
 
@@ -16,13 +19,21 @@ public class Manager : MonoBehaviour {
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehavior>();
         spawner = GameObject.FindGameObjectWithTag("Spawner").GetComponent<Spawner>();
+        time = timelimit;
+        timerText.text = "Time: " + (int)(time) + "s";
     }
 
     private void Update()
     {
+        
         int current = player.currentHealth;
         heartImage.sprite = hearts[current];
         int points = player.playerPoints;
+        if (!player.pause)
+        {
+            time -= Time.deltaTime;
+            timerText.text = "Time: " + (int)(time) + "s";
+        }
     }
 
     public void PausePlay()
